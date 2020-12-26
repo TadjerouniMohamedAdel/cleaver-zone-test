ccd5d760aa9b18e23e198119cf8d327172d5393c
adel <mohnagato@gmail.com>
2020-12-26 12:00:31 +0100

    statefull notificaltion list

--- src/components/Notification/NotificationList.js
+++ src/components/Notification/NotificationList.js
@@ -1,48 +1,56 @@
-import React from 'react'
+import React,{useState} from 'react'
 const notifications = [
     {
+      id:0,
       action:"بلال بوعيشة علّق ",
       time:"منذ 2 دقيقة",
       reaction:"comment",
       icon:"/assets/images/activity-icon.svg"
     },
     {
+      id:1,
       action:"إلياس بوجلطية تفاعل بـ",
       time:"منذ 17 دقيقة",
       reaction:"like",
       icon:"/assets/images/activity-icon_2.svg"
     },
     {
+      id:2,
       action:"بلال بوعيشة علّق ",
       time:"منذ 2 دقيقة",
       reaction:"comment",
       icon:"/assets/images/activity-icon.svg"
     },
     {
+      id:3,
       action:"بلال بوعيشة علّق ",
       time:"منذ 2 دقيقة",
       reaction:"comment",
       icon:"/assets/images/activity-icon.svg"
     },
     {
+      id:4,
       action:"إلياس بوجلطية تفاعل بـ",
       time:"منذ 17 دقيقة",
       reaction:"love",
       icon:"/assets/images/activity-icon_2.svg"
     },
     {
+      id:5,
       action:"بلال بوعيشة علّق ",
       time:"منذ 2 دقيقة",
       reaction:"comment",
       icon:"/assets/images/activity-icon.svg"
     },
     {
+      id:6,
       action:"بلال بوعيشة علّق ",
       time:"منذ 2 دقيقة",
       reaction:"comment",
       icon:"/assets/images/activity-icon.svg"
     },
     {
+      id:7,
       action:"إلياس بوجلطية تفاعل بـ",
       time:"منذ 17 دقيقة",
       reaction:"love",
@@ -51,11 +59,15 @@ const notifications = [
   
   ]
 export default function NotificationList() { 
+  const [list,setList] = useState(notifications)
+  
     return (
         <div className="section-content-notifications">
-            {notifications.map((notif,index)=>(
+            {list.length == 0&& <div style={{width:"100%",display:"flex",justifyContent:"center",fontSize:20,alignItems:"center"}}>ليس هنالك إشعارات حاليا</div>}
+            {list.map((notif,index)=>(
                 <div className="notification-card" key={index}>
                     <div>
+                        <div onClick={()=>{setList(list.filter(item=>item.id!==notif.id))}} className="close-notif"><img src={'https://cdn.iconscout.com/icon/free/png-256/close-148-457764.png'} alt="" className="Delete-Icon"/></div>
                         <img alt="" src={notif.icon} className="Activity-Icon" />
                     </div>
                     <div className="notification-content">
